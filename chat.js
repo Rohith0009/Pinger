893c7a9edcaf353cd80996ce7311e918abb3790f
Rohith M <rohithmerala@gmail.com>
2022-02-11 11:44:49 +0530

    V0.2

--- chat.js
+++ chat.js
@@ -1,8 +1,53 @@
-function startup(){
-    room_name = localStorage.getItem("room_name");
-    document.getElementById("room_name_display").innerHTML = "Welcome To, #" + room_name;
+function startup() {
+  room_name = localStorage.getItem("room_name");
+  document.getElementById("room_name_display").innerHTML = "Welcome To, #" + room_name;
 }
 
-function back(){
-    window.location = "room.html"
+function back() {
+  window.location = "room.html";
 }
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAj5LQ3TvmfykEQktNRf3ga2QcNi5T89LA",
+  authDomain: "pinger-82f60.firebaseapp.com",
+  databaseURL: "https://pinger-82f60-default-rtdb.firebaseio.com",
+  projectId: "pinger-82f60",
+  storageBucket: "pinger-82f60.appspot.com",
+  messagingSenderId: "78376631694",
+  appId: "1:78376631694:web:f8dc8170257c31979438df",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+//Main code
+
+room_name = localStorage.getItem("room_name");
+user_name = localStorage.getItem("username");
+
+function send() {
+  msg = document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
+    Name: user_name,
+    Message: msg,
+    Like: 0,
+  });
+  document.getElementById("msg").value = "";
+}
+
+function getData() {
+  firebase
+    .database()
+    .ref("/")
+    .on("value", function (snapshot) {
+      document.getElementById("room_list").innerHTML = "";
+      snapshot.forEach(function (childSnapshot) {
+        childKey = childSnapshot.key;
+        Room_names = childKey;
+        id_Room_names = Room_names.replace(" ", "_");
+        //Start code
+
+        //End code
+      });
+    });
+}
+getData();
