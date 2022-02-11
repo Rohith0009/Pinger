427fb36ca810d24a7a29d709ee4e4e2c4921085c
Rohith M <rohithmerala@gmail.com>
2022-02-11 19:26:05 +0530

    4v5q

--- JS/login.js
+++ JS/login.js
@@ -5,6 +5,9 @@ function login() {
   localStorage.setItem("full_name", full_name);
   localStorage.setItem("email", email);
   localStorage.setItem("password", password);
+  document.getElementById("full_name").innerHTML = ""
+  document.getElementById("email").innerHTML = ""
+  document.getElementById("password").innerHTML = ""
   window.location = "room.html";
 }
 
